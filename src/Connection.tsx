337caef6ebf2deba2ad91a9dd4e2ad4e5a7bdaad
David Stepanov <david.stepanovs@gmail.com>
2024-05-27 00:23:51 +0200

    works basic

---
+++ src/Connection.tsx
@@ -0,0 +1,98 @@
+import { Container, Table } from "@mantine/core";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import duration from "dayjs/plugin/duration";
+
+dayjs.extend(customParseFormat);
+dayjs.extend(duration);
+
+function ConnectionTable({ connections }) {
+  const rows = connections.map((connection) => {
+    const departure = dayjs(connection.from.departure);
+    const departureFormatted = departure.format("HH:mm");
+    const isDelayed = connection.from.delay > 0;
+    const delayDay = dayjs.duration(connection.from.delay, "minutes");
+    const delayFormatted = delayDay.format("m");
+    const expectedDeparture = dayjs(connection.from.prognosis.departure);
+    const expectedDepartureFormatted = expectedDeparture.format("HH:mm");
+
+    const arrival = dayjs(connection.to.arrival);
+    const arrivalFormatted = arrival.format("HH:mm");
+    const expectedArrival = dayjs(connection.to.arrival).add(
+      dayjs.duration(connection.from.delay, "minutes")
+    );
+    const expectedArrivalFormatted = expectedArrival.format("HH:mm");
+
+    const durationFormatted = dayjs(connection.duration, "DDdHH:mm:ss").format(
+      "m'"
+    );
+
+    return (
+      <Table.Tr key={connection.from.departure}>
+        <Table.Td>
+          {departureFormatted}
+          {isDelayed && ({ delayFormatted } = { expectedDepartureFormatted })}
+        </Table.Td>
+        <Table.Td>
+          {arrivalFormatted}
+          {isDelayed && ({ delayFormatted } = { expectedArrivalFormatted })}
+        </Table.Td>
+        <Table.Td>
+          {connection.products}
+          {connection.from.platform && " - Gl. " + connection.from.platform}
+          {" - "}
+          {durationFormatted}
+        </Table.Td>
+      </Table.Tr>
+    );
+  });
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Departure</Table.Th>
+          <Table.Th>Arrival</Table.Th>
+          <Table.Th>Details</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
+  );
+}
+
+export default function Connection({ from, to }) {
+  const [connections, setConnections] = useState([]);
+
+  const loadConnections = async () => {
+    console.log("connections", from, to);
+
+    const response = await axios.get(
+      "http://transport.opendata.ch/v1/connections",
+      {
+        params: {
+          from: from,
+          to: to,
+        },
+      }
+    );
+    const result = response.data.connections;
+    console.log("got data connections", from, to, result);
+    setConnections(result);
+  };
+
+  useEffect(() => {
+    loadConnections();
+  }, [from]);
+
+  return (
+    <Container>
+      <p>
+        {from} - {to}
+      </p>
+      <ConnectionTable connections={connections} />
+    </Container>
+  );
+}
---
+++ src/Overview.tsx
@@ -0,0 +1,97 @@
+import {
+  Flex,
+  Grid,
+  Button,
+  Text,
+  Badge,
+  Card,
+  Group,
+  Container,
+} from "@mantine/core";
+import Connection from "./Connection";
+import { useState } from "react";
+
+const GroupView = ({ group, swapConnections }) => {
+  return (
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Group justify="space-between" mt="md" mb="xs">
+        <Text fw={500}>
+          {group.from} - {group.to}
+        </Text>
+        <Button variant="subtle" onClick={(e) => swapConnections()}>
+          Opposite
+        </Button>
+      </Group>
+
+      <Flex
+        mih={50}
+        gap="md"
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+      >
+        {group.connections.map((connection) => (
+          <Connection {...connection} />
+        ))}
+      </Flex>
+    </Card>
+  );
+};
+
+const groupsOriginal = [
+  {
+    from: "Home",
+    to: "Salsa",
+    connections: [
+      { from: "Zürich, Seebach", to: "Zürich Oerlikon, Bahnhof Ost" },
+      { from: "Zürich Oerlikon", to: "Zürich Hardbrücke" },
+    ],
+  },
+  {
+    from: "Home",
+    to: "HB",
+    connections: [
+      { from: "Zürich, Seebach", to: "Zürich Oerlikon, Bahnhof Ost" },
+      { from: "Zürich Oerlikon", to: "Zürich HB" },
+    ],
+  },
+  {
+    from: "Home",
+    to: "Altstetten",
+    connections: [
+      { from: "Zürich, Seebach", to: "Zürich Oerlikon, Bahnhof Ost" },
+      { from: "Zürich Oerlikon", to: "Zürich Altstetten" },
+    ],
+  },
+];
+
+function swapFromTo(groups) {
+  return groups.map((group) => {
+    return {
+      from: group.to,
+      to: group.from,
+      connections: group.connections.reverse().map((connection) => ({
+        from: connection.to,
+        to: connection.from,
+      })),
+      //   connections: group.connections,
+    };
+  });
+}
+
+export default function Overview() {
+  const [groups, setGroups] = useState(groupsOriginal);
+  const swapConnections = () => {
+    const swappedGroups = swapFromTo(groups);
+    setGroups(swappedGroups);
+  };
+
+  return (
+    <Container>
+      {groups.map((group) => (
+        <GroupView group={group} swapConnections={swapConnections} />
+      ))}
+    </Container>
+  );
+}
